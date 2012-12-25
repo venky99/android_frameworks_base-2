9d3d3c0501062a9bd3aa726df5ae583a7325cb49
Daveee10 <daver16@msn.com>
2012-12-25 13:10:16 +0100

    SEMC Stock FM Radio app support

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -419,6 +419,11 @@ public class AudioManager {
      */
     public static final int USE_DEFAULT_STREAM_TYPE = Integer.MIN_VALUE;
 
+    /**
+     * Showing whether we are routing FM Radio to speaker or not. Used by stock SEMC FM Radio app
+     */
+    private static boolean FmRadioSpeaker = true;
+
     private static IAudioService sService;
 
     /**
@@ -1163,6 +1168,17 @@ public class AudioManager {
         }
     }
 
+    public void setSpeakerMediaOn(boolean on){
+        Log.e(TAG, "Setting speakerMediaon");
+        if (on)
+            setParameters("fm_radio_speaker=on");
+        else
+            setParameters("fm_radio_speaker=off");
+
+        FmRadioSpeaker=on;
+        playSoundEffect(FX_KEY_CLICK, -1);
+    }
+
     /**
      * Checks whether the speakerphone is on or off.
      *
@@ -1177,6 +1193,14 @@ public class AudioManager {
             return false;
         }
      }
+
+    /**
+     * Checks whether the speakerphone is on or off.
+     * @return true if speakerphone is on, false if it's off
+     */
+    public boolean isSpeakerMediaOn() {
+        return FmRadioSpeaker;
+    }
 
     //====================================================================
     // Bluetooth SCO control
