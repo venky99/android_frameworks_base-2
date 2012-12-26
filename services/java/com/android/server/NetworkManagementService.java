77a7445661de0f06bcbf57a8b0f717f7bd0caca5
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2012-12-26 17:51:45 +0400

    Support devices with SoftAp that don't require firmware reload
    Change-Id: I406daf3d7c833f034d78588c21cd5074b4db523b
    
    Conflicts:
    	core/res/res/values/symbols.xml
    	services/java/com/android/server/NetworkManagementService.java
    
    Conflicts:
    	services/java/com/android/server/NetworkManagementService.java

--- services/java/com/android/server/NetworkManagementService.java
+++ services/java/com/android/server/NetworkManagementService.java
@@ -1018,11 +1018,13 @@ public class NetworkManagementService extends INetworkManagementService.Stub
             WifiConfiguration wifiConfig, String wlanIface) {
         mContext.enforceCallingOrSelfPermission(CONNECTIVITY_INTERNAL, TAG);
         try {
-            if (mContext.getResources().getBoolean(
+            Resources resources = mContext.getResources();
+            if (resources.getBoolean(
                         com.android.internal.R.bool.config_wifiApFirmwareReload)) {
                 wifiFirmwareReload(wlanIface, "AP");
             }
-            if (mContext.getResources().getBoolean(
+
+            if (resources.getBoolean(
                         com.android.internal.R.bool.config_wifiApStartInterface)) {
                 mConnector.execute("softap", "start", wlanIface);
             }
