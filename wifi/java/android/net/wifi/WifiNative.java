33de9c02a3db4bf33e871485973ca9d048014c86
Bruno Randolf <br1@einfach.org>
2013-02-28 23:19:28 +0000

    Wifi: Add IBSS supported method
    Add method isIbssSupported() to WifiManager and related classes to query wether
    the driver supports IBSS mode. The query is answered by wpa_supplicants new
    command "get_capability modes".
    
    It will only return a correct result after WIFI_STATE_ENABLED has been
    reached.
    
    Change-Id: I93aa2fdff94cd897c91ec3beb7e58940d1de7b72
    
    Conflicts:
    	wifi/java/android/net/wifi/WifiNative.java

--- wifi/java/android/net/wifi/WifiNative.java
+++ wifi/java/android/net/wifi/WifiNative.java
@@ -798,4 +798,18 @@ public class WifiNative {
     public boolean p2pServDiscCancelReq(String id) {
         return doBooleanCommand("P2P_SERV_DISC_CANCEL_REQ " + id);
     }
+
+    public boolean getModeCapability(String mode) {
+        String ret = doStringCommand("GET_CAPABILITY modes");
+        if (!TextUtils.isEmpty(ret)) {
+            String[] tokens = ret.split(" ");
+            for (String t : tokens) {
+                if (t.compareTo(mode) == 0)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public native static boolean setMode(int mode);
 }
